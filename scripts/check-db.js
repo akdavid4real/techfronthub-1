2f7cc5f5edfebbfad9dc5106e50831d7349375d5
akdavid <techfronthub@gmail.com>
2026-05-03 03:21:52 +0100

    space

--- scripts/check-db.js
+++ scripts/check-db.js
@@ -3,6 +3,7 @@ import { Pool } from 'pg'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 dotenv.config({ path: path.resolve(__dirname, '../.env.local') })
 
